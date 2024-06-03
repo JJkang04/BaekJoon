dc7a27d8bb5a5b9d2644b1ca776d89e3d6660b0e
JJkang04 <151365583+JJkang04@users.noreply.github.com>
2024-06-03 23:06:43 +0900

    [Silver II] Title: 베르트랑 공준, Time: 52 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+#include <vector>
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	while (true) {
+		int n;
+		cin >> n;
+		
+		if (n == 0) {
+			break;
+		}
+
+		vector<bool> isPrime(2*n+1, true); //탐색 범위가 2n까지이므로 벡터의 크기는 2n+1
+		
+		isPrime[0] = isPrime[1] = false; //0과 1은 기본적으로 false 처리 - 에라토스테네스의 체
+
+		int countPrime = 0;
+
+		for (ll i = 2; i <= 2 * n; ++i) {
+			if (isPrime[i]) { //여기서 true인 것이 소수. 이 수의 배수(2n보다 작거나 같은)을 false처리
+				if (i > n) {
+					countPrime++;
+				}
+				for (ll j = i * i; j <= 2 * n; j += i) {
+					isPrime[j] = false;
+				}
+			}
+		}
+		cout << countPrime << '\n';
+	}
+
+	return 0;
+}
