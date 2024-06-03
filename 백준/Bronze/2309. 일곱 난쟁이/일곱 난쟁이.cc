fd79d7dd81b678944271ddc58dcddcb90e4b7a1e
JJkang04 <151365583+JJkang04@users.noreply.github.com>
2024-06-04 05:48:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <set>
+#include <algorithm>
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	vector<int> vec(9);
+
+	for (int i = 0; i < 9; ++i) {
+		int h;
+		cin >> h;
+		vec[i] = h;
+	}
+
+	sort(vec.begin(), vec.end());
+
+	for (int i = 0; i < 9; ++i) {
+		for (int j = i + 1; j < 9; ++j) {
+			int sum = 0;
+			for (int k = 0; k < 9; ++k) {
+				if (k == i || k == j) {
+					continue;
+				}
+				else {
+					sum += vec[k];
+				}
+			}
+			if (sum == 100) {
+				int count = 0;
+				for (int l = 0; l < 9; ++l) {
+					if (l == i || l == j) {
+						continue;
+					}
+					count++;
+					if (count == 7) {
+						cout << vec[l];
+						return 0;
+					}
+					cout << vec[l] << '\n';
+					}
+				}
+			}
+		}
+
+	return 0;
+}
