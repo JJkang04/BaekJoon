2bc3d8366dc21cb6817fced990b5cb1caa8aa1c3
JJkang04 <151365583+JJkang04@users.noreply.github.com>
2024-06-04 18:09:36 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cmath>
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int space = 2 * n - 1;
+
+	for (int i = 1; i <=n; ++i) {
+		int star = 2 * i - 1;
+		for (int j = 0; j < (space - star) / 2; ++j) {
+			cout << ' ';
+		}
+		for (int k = 0; k < star; ++k) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+
+
+	return 0;
+}
