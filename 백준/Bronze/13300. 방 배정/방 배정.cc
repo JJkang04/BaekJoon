a6db556a4e36dc1f2dc3b41708e6b1c0e6bfccf9
JJkang04 <151365583+JJkang04@users.noreply.github.com>
2024-06-04 22:26:26 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cmath>
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+
+int f_array[7] = {};
+int m_array[7] = {};
+
+int main() {
+	
+	int n;
+	cin >> n;
+
+	int k;
+	cin >> k;
+
+	bool gen;
+
+	int grade;
+
+	for (int i = 0; i < n; ++i) {
+		cin >> gen >> grade;
+		if (gen) m_array[grade]++;
+		else f_array[grade]++;
+	}
+
+	int ans = 0;
+
+	for (int i = 1; i < 7; ++i) {
+		ans += ceil(f_array[i] / 2.0);
+		ans += ceil(m_array[i] / 2.0);
+	}
+	cout << ans;
+
+	return 0;
+	
+}
