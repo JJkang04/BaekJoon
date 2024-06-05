b7b24813e7e70e24432c1cff048d288d1e546d8b
JJkang04 <151365583+JJkang04@users.noreply.github.com>
2024-06-05 21:56:35 +0900

    [Silver II] Title: 에디터, Time: 44 ms, Memory: 11088 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -13,51 +13,91 @@ using namespace std;
 
 typedef long long ll;
 
+const int MX = 1000005;
+char dat[MX];
+int pre[MX];
+int nxt[MX];
+int unused = 1;
+
+void insert(int addr, char ch) {
+    dat[unused] = ch;
+    nxt[unused] = nxt[addr];
+    pre[unused] = addr;
+
+    if (nxt[addr] != -1) {
+        pre[nxt[addr]] = unused;
+    }
+
+    nxt[addr] = unused;
+    unused++;
+}
+
+void erase(int addr) {
+    nxt[pre[addr]] = nxt[addr];
+    
+    if (nxt[addr] != -1) {
+        pre[nxt[addr]] = pre[addr];
+    }
+}
+
+void traversal() {
+    int cur = nxt[0];
+    while (cur != -1) {
+        cout << dat[cur];
+        cur = nxt[cur];
+    }
+}
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    list<char> L;
+    fill(pre, pre + MX, -1);
+    fill(nxt, nxt + MX, -1);
+
     string str;
     cin >> str;
 
+    int cursor = 0;
+
     for (const char& c : str) {
-        L.push_back(c);
+        insert(cursor, c);
+        cursor++;
     }
-    auto cursor = L.end();
+
     int t;
     cin >> t;
 
-    for (int i = 0; i < t; ++i) {
-        
+    while (t--) {
         char c;
         cin >> c;
+
         if (c == 'P') {
             char ch;
             cin >> ch;
-            L.insert(cursor,ch);
+
+            insert(cursor, ch);
+            cursor = nxt[cursor];
         }
         else if (c == 'L') {
-            if (cursor != L.begin()) {
-                cursor--;
+            if (pre[cursor] != -1) {
+                cursor = pre[cursor];
             }
         }
         else if (c == 'D') {
-            if (cursor != L.end()) {
-                cursor++;
+            if (nxt[cursor] != -1) {
+                cursor = nxt[cursor];
             }
         }
         else if (c == 'B') {
-            if (cursor != L.begin()) {
-                cursor--;
-                cursor  = L.erase(cursor);
+            if (pre[cursor] != -1) {
+                erase(cursor);
+                cursor = pre[cursor];
             }
         }
     }
 
-    for (auto i : L) {
-        cout << i;
-    }
+    traversal();
+
     return 0;
 
 }
