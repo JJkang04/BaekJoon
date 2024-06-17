9da3bdc904e06812140cd3a58e4dbd6e9e50e4cb
JJkang04 <151365583+JJkang04@users.noreply.github.com>
2024-06-17 20:45:14 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4344 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <set> 
+#include <algorithm>
+#include <list>
+#include <stack>
+#include <queue>
+#define endl '\n'
+
+typedef long long ll;
+
+using namespace std;
+
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+
+    vector<int> vec;
+
+    int k;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> k;
+        vec.push_back(k);
+    }
+
+    sort(vec.begin(), vec.end());
+   
+    for (const int& i : vec) {
+        cout << i << '\n';
+    }
+
+    return 0;
+}
