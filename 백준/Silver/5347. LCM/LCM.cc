964a0a94d4e48e1bbbaf8fb668ddfa3d47ebe473
JJkang04 <151365583+JJkang04@users.noreply.github.com>
2024-09-03 20:14:29 +0900

    [Silver V] Title: LCM, Time: 8 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		string line;
+		getline(cin, line);
+
+		istringstream iss(line);
+		int a, b;
+		cin >> a >> b;
+
+		if (a == b) {
+			cout << a << '\n';
+			continue;
+		}
+
+		int big, small;
+
+		if (a > b) {
+			big = a;
+			small = b;
+		}
+		else {
+			big = b;
+			small = a;
+		}
+
+		int mul = 1;
+
+		while (true) {
+			long long int temp = big;
+			temp *= mul;
+			if (temp % small == 0) {
+				cout << temp << '\n';
+				break;
+			}
+			mul++;
+		}
+	}
+}
